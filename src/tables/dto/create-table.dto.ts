0c87d73a9ec9ec1e07dcea73aa31295d7b221abd
ElenaStr13 <estrateluk@yahoo.com>
2025-05-05 14:18:58 +0300

    lesson 1

---
+++ src/tables/dto/create-table.dto.ts
@@ -0,0 +1,34 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNumber,
+  IsOptional,
+  IsString,
+  Max,
+  MaxLength,
+  Min,
+  MinLength,
+} from 'class-validator';
+
+export class CreateTableDto {
+  @ApiProperty({ example: 'wood' })
+  @IsString()
+  @MinLength(2)
+  @MaxLength(255)
+  type: string;
+
+  @ApiProperty({ example: 10 })
+  @IsNumber()
+  @Min(10)
+  @Max(1_000_000)
+  width: number;
+
+  @ApiProperty({ example: 10 })
+  @IsNumber()
+  @Min(10)
+  @Max(1_000_000)
+  height: number;
+
+  @ApiProperty({ example: true })
+  @IsOptional()
+  inStock: boolean;
+}
